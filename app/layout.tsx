b041393acfab384f9e9f2c254fadbcd71188b41b
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-02-04 17:23:10 +0530

    :bento: My Next Starter Kit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Starter Kit by Nisarg Thakkar",
+  description: "Next.js Starter Kit",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full text-5xl font-bold md:text-6xl lg:text-7xl flex mt-28 justify-center text-center">
+      Hello World
+    </div>
+  );
+}
